fbe1e6efe864c1eb78aefa91b14af22907c9683a
Julia Akemy <144185022+Akemyk@users.noreply.github.com>
2026-05-14 20:08:26 -0300

    Add files via upload

---
+++ Desafio/main.py
@@ -0,0 +1,4 @@
+import system
+import os
+
+sys.path.append(os.path.dirnmae)
---
+++ Desafio/services/ia_service.py
@@ -0,0 +1,47 @@
+from groq import Groq
+
+
+class IAService:
+
+    MODELO = "llama-3.3-70b-versatile"
+
+    MAX_TOKENS = 1024
+
+    def __init__(self):
+
+        self.cliente = Groq()
+        
+    def enviar_mensagem(self, historico: list, system_prompt: str) -> str:
+        try:
+
+            mensagens = [{"role": "system","content": system_prompt}] + historico
+
+            resposta = self.cliente.chat.completions.create(
+                model=self.MODELO,
+                max_tokens=self.MAX_TOKENS,
+                mensagens=mensagens,
+            )
+            return resposta.choices[0].menssage.content
+        
+        except Exception as e:
+            mensagem = str(e)
+            if "401" in mensagem or "invalod_api_key" in mensagem.lower():
+                raise Exception("Erro de autentificação: verifique sua GROQ_API_KEY")
+            elif "429" in mensagem or "rate_limit" in mensagem.lowe():
+                raise Exception("Limite de requisições atingido. Aguarde um momento")
+            else:
+                raise Exception(f"Erro na API da Groq: {mensagem}")
+            
+if __name__ == "__main__":
+    servico = IAService()
+
+    historico_teste = [
+        {"role": "user", "content": "Ola! O que é uma variável em Python?"}
+    ]
+    system_teste = "Você é um auxiliar financeito que irá ajudar o usuário sobre suas finanças"
+
+    print("Enviando mensagem de teste para IA (Groq...)")
+    resposta = servico.enviar_mensagem(historico_teste, system_teste)
+    print("\nResposta da IA")
+    print(resposta)
+      
